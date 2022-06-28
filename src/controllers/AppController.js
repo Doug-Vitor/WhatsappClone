aed9e74fcc3cd51d89f3185d9ff1dc3d7b57794e
Douglas <douglasvitor.dev@gmail.com>
2022-06-28 11:15:55 -0300

    Section 7.3: Prototype to reuse code

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -1,6 +1,7 @@
 class AppController {
     constructor() {
         this.loadElements();
+        this.elementsPrototype();
     }
 
     loadElements() {
@@ -8,5 +9,23 @@ class AppController {
         document.querySelectorAll('[id]').forEach(element => {
             this.elements[Format.getCamelCase(element.id)] = element;
         });
+    }
+
+    elementsPrototype() {
+        Element.prototype.toggle = function() {
+            this.style.display = this.style.display == 'block' ? 'none' : 'block';
+        };
+
+        Element.prototype.on = function(events, fn) {            
+            events.split(' ').forEach(event => {
+                this.addEventListener(event, fn);
+            });
+        }
+
+        Element.prototype.css = function(styles) {
+            for (let name in styles) {
+                this.style[name] = styles[name];
+            }
+        }
     }
 }
--- src/utils/Format.js
+++ src/utils/Format.js
@@ -4,4 +4,33 @@ class Format {
         div.innerHTML = `<div data-${text}="id"></div>`;
         return Object.keys(div.firstChild.dataset)[0];
     }
+
+    static elementsPrototype() {
+        Element.prototype.toggle = function() {
+            this.style.display = this.style.display == 'block' ? 'none' : 'block';
+            return this;
+        };
+
+        Element.prototype.on = function(events, fn) {            
+            events.split(' ').forEach(event => {
+                this.addEventListener(event, fn);
+            });
+
+            return this;
+        }
+
+        Element.prototype.css = function(styles) {
+            for (let name in styles) {
+                this.style[name] = styles[name];
+            }
+
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(className) {
+            this.classList.toggle(className);
+            
+            return this;
+        }
+    }
 }
